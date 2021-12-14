14d4acd7d1de6468967c769fac3d9fdc6d250be3
yesong choi <to06109@naver.com>
2021-12-15 05:09:46 +0900

    상세정보 2차 완성

--- src/routes/home/goods.js
+++ src/routes/home/goods.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
-// //db 불러옴
-// const db = require("../../config/db");
-// var router = express.Router();
+//db 불러옴
+const db = require("../../config/db");
+var router = express.Router();
 
 
 //ctrl 받아오기
 const ctrl = require("./home.ctrl");
 
 router.get('/detail', ctrl.output.detail);
-router.post('/detail', ctrl.process.detail);
+//router.post('/detail', ctrl.process.detail);
+
+// router.post('/email_post', function(req, res) {
+//     res.render('email.ejs', {'email' : req.body.email})
+// })
+
+router.post('/search_post', function(req, res) {
+    const keyword = req.body.keyword;
+    const sql = "SELECT  Name, Price, SHOPBASKET.Desc, Location, Image FROM SHOPBASKET where Name = ?;"
+    db.query(sql, [keyword], (err, result, fields) => {
+        if (err) throw err;
+        //res.send(result);
+        //console.log(result);
+
+        res.render('search.ejs', {'Name' : result[0].Name, 'Price' : result[0].Price, 'Desc' : result[0].Desc, 'Location' : result[0].Location, 'Image' : result[0].Image})//검색어 받아옴
+    });
+})
+
 //router.get('/test', ctrl.output.test);
 
 // router.get("/search", (req, res) => {
